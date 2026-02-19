88e03f48684626930a5b68cd2880033e2102836e
iffyloop <35245220+iffyloop@users.noreply.github.com>
2026-02-19 02:06:29 -0600

    Update .sh

--- interactive_inference.sh
+++ interactive_inference.sh
@@ -2,4 +2,4 @@ torchrun \
   --nproc_per_node=1 \
   --master_port=29500 \
   interactive_inference.py \
-  --config_path configs/longlive_interactive_inference.yaml
+  --config_path configs/longlive_interactive_ttm_camcontrol_concertstage.yaml
